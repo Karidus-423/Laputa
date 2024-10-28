81860bf670c4662c8d12ebdc59783c9ca3dc17a5
Karidus-423 <kapuerto23@gmail.com>
2024-10-28 18:44:30 -0400

    init commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "zig development flake";
+
+  # Flake inputs
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+  };
+
+  # Flake outputs
+  outputs = { self, nixpkgs, flake-utils}:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+        pkgs = import nixpkgs {
+			system = system;
+			config.allowUnfree = true;
+			};
+    in
+    {
+		formatter = pkgs.nixpkgs-fmt;
+        devShells.default = pkgs.mkShell {
+			name = "Raylib Gamejam";
+			# Programs used during runtime
+            buildInputs = with pkgs;[
+			clang-tools
+			just
+			raylib
+            			];
+			#shellHook = '' '';
+		};
+    });
+}
+
